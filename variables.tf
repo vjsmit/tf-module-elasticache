896ab1ebccbc3f1a6e03754dea7e2602e0ce4f34
vjsmit <vj.smit97@gmail.com>
2023-08-04 00:02:33 +0530

    updating userdata.sh

--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "subnet_ids" {}
 variable "tags" {}
 variable "sg_subnets_cidr" {}
 variable "kms_key_arn" {}
+variable "vpc_id" {}
+variable "engine" {}
+variable "node_type" {}
+variable "engine_version" {}
+variable "port" {
+  default = 6379
+}
+variable "replicas_per_node_group" {}
+variable "num_node_groups" {}
