a1f0a71b97e2e3a5bf9d1f82b585f4e1554a72b8
vjsmit <vj.smit97@gmail.com>
2023-08-26 20:07:52 +0530

    Build all app components

--- main.tf
+++ main.tf
@@ -55,4 +55,6 @@ resource "aws_elasticache_cluster" "instance" {
   parameter_group_name = "default.redis6.x"
   engine_version       = var.engine_version
   port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
+  security_group_ids   = [ aws_security_group.main.id ]
 }
